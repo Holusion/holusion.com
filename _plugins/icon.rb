4e116c3888ede14c457aa730b448dbf9c0735cd1
Sebastien Dumetz <s.dumetz@holusion.com>
2017-01-04 16:54:51 +0100

    fix some icons where xmlns was missing. Add a new icon liquid tag helper

---
+++ _plugins/icon.rb
@@ -0,0 +1,34 @@
+module Jekyll
+  class Icon < Liquid::Tag
+    def initialize(tag_name, input, tokens)
+      super
+      # we split on every = then take a substring on last space.
+      # ie : we're fine as long as param keys are 1 word.
+      params = input.split("=")
+      @name = params[0][0..params[0].rindex(" ")].strip
+      size = /size=(\d*)/.match(input)
+      if size && size[1]
+        @width=size[1]
+      else
+        @width=24
+      end
+      style =  /style=["']?(.*)(["']|$)/.match(input)
+      if style && style[1]
+        @style=%(style="#{style[1]}")
+      else
+        @style=""
+      end
+    end
+
+    def render(context)
+      %(
+      <svg fill="#ffffff" height="#{@width}" viewBox="0 0 24 24" width="#{@width}" #{@style} xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink">
+        <use xlink:href="/static/icons-combined.svg##{@name}"></use>
+      </svg>
+      )
+    end
+  end
+end
+
+Liquid::Template.register_tag('icon', Jekyll::Icon)
