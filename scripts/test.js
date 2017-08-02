3e8593629c33737e6f411b33b6802c048dcfce68
segolenedhm <s.duhamel@holusion.com>
2017-08-02 16:07:16 +0200

    Merge branch 'testing' of https://github.com/Holusion/holusion.com into testing
    update navbar style to stop ugly line break on medium screen. Add store menu item
    
    major rework of store pages WIP
    
    correct url for store page testing
    
    fix some layout and overflows in new store format
    
    add every products to fr store
    
    fix item link name
    
    update build script to provide integration testing
    
    update absolute_url filter to provide good URL to snipcart
    
    Store pixel update
    
    Fiches store update
    
    create store index page for english version
    
    Store en
    
    fix yaml in pixel.html header
    
    update opal header images
    
    use #content class for store page
    
    update config
    
    use #content id for store main
    
    update integration tests to use selenium-webdriver
    
    temporary fixes for bottom navbar layout inconsistencies on small displays
    
    shorten copyright string
    
    fix JSON parse error
    
    update slider using IIS
    
    use jpg picture for section background to lighten pages
    
    check in new front sectiopn image
    
    limit the size of header images in store
    
    add placeholder pictos to products page on store for large displays
    
    add an option for no-padding rows on small devices targetting full-width medias
    
    fix incon alignment on store products page
    
    correct spelling mistakes in french store
    
    correct typo in opalv en store page
    
    improve shipping indication
    
    shorten pixel XL description
    
    tidy up build compression
    
    New vignette pictures opal
    
    picture opal v rename
    
    Update opal productsheet
    
    rename picture
    
    fix post slider for stories section
    
    padding picture
    
    update opal picture
    
    picture update
    
    fix en posts slider
    
    Opal productsheet update
    
    Opal update
    
    Lien store
    
    Update opal & home
    
    Update opal + home
    
    Update opal video youtube
    
    Update english opal version
    
    center picture
    
    Pictures opal product
    
    add images
    
    modif ai
    
    Finalisation page Opal fr
    
    Opal update en
    
    various grammatical and syntax corrections
    
    update thumbnails in store to include product price
    
    fix some display inconsistencies on top navbar. Resize language picker
    
    various minor accessibility updates
    
    update
    
    picture update
    
    Images slider pixel
    
    Update vignettes produits
    
    add integration test for snipcart required property
    
    rework style of store header

---
+++ scripts/test.js
@@ -0,0 +1,107 @@
+'use strict';
+const selenium = require ('selenium-webdriver');
+const By = selenium.By;
+const until = selenium.until;
+const expect = require("chai").expect;
+require("chai").should();
+const jsdom = require("jsdom");
+
+const url = require('url').Url;
+
+var browser = process.env["BROWSER"]||"phantomjs";
+
+let driver = new selenium.Builder().forBrowser(browser).build();
+let target = process.argv[3]||process.env["TARGET"];
+
+
+
+
+describe(`Test : ${target}`, function(){
+  this.slow(2000);
+  this.timeout(6000);
+  before(function(){
+    expect(target).to.be.ok;
+  });
+  after(function(){
+    driver.quit();
+  });
+  describe("HTTPS",()=>{
+    let l_target = new url("/");
+    l_target.protocol = "https";
+    l_target.hostname = target;
+    let href = l_target.format();
+    ["fr","en"].forEach((lang)=>{
+      [
+        `${href}/${lang}/`,
+        `${href}/${lang}`,
+        `${href}/${lang}/index`,
+        `${href}/${lang}/index.html`
+      ].forEach((l)=>{
+        it(`GET ${l}`,()=>{
+          return driver.get(`${l}`)
+          .then(()=>driver.getTitle())
+          .then((title)=> title.should.match(/holusion/))
+        });
+      })
+
+
+      const store_products = {
+        "pixel":{name:"Pixel"},
+        "pixelxl":{name:"Pixel XL"},
+        "pixelplus":{name:"Pixel +"},
+        "opalv":{name:"Opal"},
+        "opalh":{name:"Opal"}
+      }
+      it(`GET ${href}/${lang}/store/`,()=>{
+        return driver.get(`${href}/${lang}/store/`)
+        .then(_=>driver.getTitle())
+        .then((title)=> title.should.match(/(store|boutique)/i))
+        .then(_=> driver.findElements(By.css(".thumbnail>a")))
+        .then((thumbs)=>{
+          expect(thumbs).to.have.property("length").above(0);
+          //We start an iteration on each shop page from shop index list
+          return Promise.all(thumbs.map(t=> t.getAttribute("href")))
+        })
+        .then((hrefs)=>{
+          Object.keys(store_products).forEach(function(p) {
+            expect(p).to.satisfy(function(p){
+              return hrefs.some(function(link){
+                return link.indexOf(`/${lang}/store/p`) != -1;
+              })
+            }, `expect every poroduct to be in store index. Missing "${p}"`)
+          })
+        })
+      });
+
+      Object.keys(store_products).forEach((product)=>{
+        let pageLink = `${href}/${lang}/store/${product}`;
+        describe(`GET ${pageLink}`,()=>{
+          let page;
+          before(function(){
+            page = driver.get(pageLink);
+            return page; //trigger wait until it's done.
+          })
+          it("Has product name in title",function(){
+            return page.then(_=>driver.getTitle())
+            .then((title)=>title.should.match(new RegExp(store_products[product].name,"i")))
+          })
+          it("Have snipcart properties",function(){
+            return page.then(_=>driver.findElement(By.css(".button.snipcart-add-item")))
+            .then((btn)=>{
+              return Promise.all([
+                btn => btn.getAttribute("data-item-name")
+                  .then((name)=>name.should.be.a.string),
+                btn => btn.getAttribute("data-item-price")
+                  .then((name)=>name.should.match(/[0-9.]+/)),
+                btn => btn.getAttribute("data-item-url")
+                  .then((name)=>name.should.equal(pageLink)),
+                btn => btn.getAttribute("data-item-description")
+                  .then((name)=>name.should.be.a.string.of.length.above(20),
+               ]);
+             });
+          });
+        });
+      });
+    });
+  });
+})
