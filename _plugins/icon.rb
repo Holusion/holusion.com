b986df383fbaa93477ce1b5d2a16db84f3091fea
Sebastien Dumetz <s.dumetz@holusion.com>
2017-08-18 11:29:15 +0200

    deprecate the use of icon plugin, in favor of inline svg. Add support for more svg properties. Restyle store cart button

--- _plugins/icon.rb
+++ _plugins/icon.rb
@@ -6,23 +6,21 @@ module Jekyll
       # ie : we're fine as long as param keys are 1 word.
       params = input.split("=")
       @name = params[0][0..params[0].rindex(" ")].strip
-      size = /size=(\d*)/.match(input)
-      if size && size[1]
-        @width=size[1]
-      else
-        @width=24
-      end
-      style =  /style=["']?(.*)(["']|$)/.match(input)
-      if style && style[1]
-        @style=%(style="#{style[1]}")
-      else
-        @style=""
-      end
+
+      # Parse parameters
+      size = /size\s*=\s*(\d*)/.match(input)
+      @width = (size && size[1])? size[1] : 24
+
+      style =  /style\s*=\s*["']?(.+)(["']|$)/.match(input)
+      @style = (style && style[1]) ? %(style="#{style[1]}") : ""
+
+      fill = /fill=\s*["']?(#[0-9abcdefABCDEF]+|none)(["']|$)/.match(input)
+      @fill = (fill && fill[1]) ? fill[1] : "#ffffff"
     end
 
     def render(context)
       %(
-      <svg aria-label="#{@name}" fill="#ffffff" height="#{@width}" viewBox="0 0 24 24" width="#{@width}" #{@style} xmlns="http://www.w3.org/2000/svg"
+      <svg aria-label="#{@name}" fill="#{@fill}" height="#{@width}" viewBox="0 0 24 24" width="#{@width}" #{@style} xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
         <use xlink:href="##{@name}"></use>
       </svg>
