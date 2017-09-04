6659fc9247ea477df4df3af2d99aacb25996ed54
Sebastien Dumetz <s.dumetz@holusion.com>
2017-09-04 17:16:14 +0200

    validate titles length in tests. update product and store pages titles

--- scripts/test.js
+++ scripts/test.js
@@ -37,7 +37,7 @@ function get_sizes(items){
   });
 }
 
-describe(`Test : ${href}`, function(){
+describe(`${href} :`, function(){
   this.slow(2000);
   this.timeout(6000);
   before(function(){
@@ -46,107 +46,115 @@ describe(`Test : ${href}`, function(){
   after(function(){
     driver.quit();
   });
-  describe(u.protocol,()=>{
 
-    ["fr","en"].forEach((lang)=>{
-      [
-        `${href}/${lang}/`,
-        `${href}/${lang}`,
-        `${href}/${lang}/index`,
-        `${href}/${lang}/index.html`
-      ].forEach((l)=>{
-        it(`GET ${l}`,()=>{
-          return driver.get(`${l}`)
-          .then(()=>driver.getTitle())
-          .then((title)=> title.should.match(/holusion/))
-        });
-      })
-
-
-      const store_products = {
-        "pixel":{name:"Pixel"},
-        "pixelxl":{name:"Pixel XL"},
-        "pixelplus":{name:"Pixel +"},
-        "opalv":{name:"Opal"},
-        "opalh":{name:"Opal"}
-      }
-      describe(`GET ${href}/${lang}/store/`,()=>{
-        let page
-        before(()=>{
-          page =driver.get(`${href}/${lang}/store/`);
-          return page;
-        })
-        it("has a title",()=>{
-          return page.then(_=>driver.getTitle())
-          .then((title)=> title.should.match(/(store|boutique)/i))
-        })
-        it("has thumbnails",()=>{
-          return page.then(_=> driver.findElements(By.css(".thumbnail>a")))
-          .then((thumbs)=>{
-            expect(thumbs).to.have.property("length").above(0);
-            //We start an iteration on each shop page from shop index list
-            return Promise.all(thumbs.map(t=> t.getAttribute("href")))
-            .then((hrefs)=>{
-              //Ensure we don't get products list wrong
-              let match_names = Object.keys(store_products).join("|");
-              let r = new RegExp(`^${href}/${lang}/store/(${match_names})`);
-              hrefs.forEach((link)=>{
-                expect(link).to.match(r);
-              })
-              expect(hrefs).to.have.property("length",Object.keys(store_products).length);
-              return hrefs;
-            })
-          })
+  ["fr","en"].forEach((lang)=>{
+    [
+      `${href}/${lang}/`,
+      `${href}/${lang}`,
+      `${href}/${lang}/index`,
+      `${href}/${lang}/index.html`
+    ].forEach((l)=>{
+      it(`GET ${l}`,()=>{
+        return driver.get(`${l}`)
+        .then(()=>driver.getTitle())
+        .then((title)=> {
+          expect(title).to.match(/holusion/i);
         })
       });
-      //Since we verified in previous test that "store_products" is acurate
-      //We can base further store tests on it
-      describe("test store products",function(){
-        Object.keys(store_products).forEach((product)=>{
-          let pageLink = `${href}/${lang}/store/${product}`;
-          describe(`${store_products[product].name}`,()=>{
-            let page;
-            before(function(){
-              page = driver.get(pageLink);
-              return page; //trigger wait until it's done.
-            })
-            it("Has product name in title",function(){
-              return page.then(_=>driver.getTitle())
-              .then((title)=>title.should.match(new RegExp(store_products[product].name,"i")))
-            })
-            it("Have snipcart properties",function(){
-              return page.then(_=>driver.findElement(By.css(".button.snipcart-add-item")))
-              .then((btn)=>{
-                return Promise.all([
-                  btn => btn.getAttribute("data-item-name")
-                    .then((name)=>name.should.be.a.string),
-                  btn => btn.getAttribute("data-item-price")
-                    .then((name)=>name.should.match(/\d+\.\d{2}/)),
-                  btn => btn.getAttribute("data-item-url")
-                    .then((name)=>name.should.equal(pageLink)),
-                  btn => btn.getAttribute("data-item-description")
-                    .then((name)=>name.should.be.a.string.of.length.above(20)),
-                 ]);
-               });
-            });
-          });
-        });
-      })
+    })
 
-      /* Check thumbnails */
-      describe("check thumbnails size",function(){
-        [
-          `${href}/${lang}/products/`,
-          `${href}/${lang}/store/`,
-          `${href}/${lang}/posts/`,
-        ].forEach(function(link){
-          it(`on ${link}`,function(){
-            return driver.get(`${link}`)
-            .then(_=> driver.findElements(By.css(".thumbnail>a")))
-            .then((thumbs)=> get_sizes(thumbs))
+
+    const store_products = {
+      "pixel":{name:"Pixel"},
+      "pixelxl":{name:"Pixel XL"},
+      "pixelplus":{name:"Pixel +"},
+      "opalv":{name:"Opal"},
+      "opalh":{name:"Opal"}
+    }
+    describe(`GET ${href}/${lang}/store/`,()=>{
+      let page
+      before(()=>{
+        page =driver.get(`${href}/${lang}/store/`);
+        return page;
+      })
+      it("has a title with reasonable length",()=>{
+        return page.then(_=>driver.getTitle())
+        .then((title)=> {
+          expect(title).to.match(/(store|boutique)/i);
+          /* recommended length is in fact <70 but let's not be too conservative */
+          expect(title).to.have.lengthOf.within(25,120);
+        })
+      })
+      it("has thumbnails",()=>{
+        return page.then(_=> driver.findElements(By.css(".thumbnail>a")))
+        .then((thumbs)=>{
+          expect(thumbs).to.have.property("length").above(0);
+          //We start an iteration on each shop page from shop index list
+          return Promise.all(thumbs.map(t=> t.getAttribute("href")))
+          .then((hrefs)=>{
+            //Ensure we don't get products list wrong
+            let match_names = Object.keys(store_products).join("|");
+            let r = new RegExp(`^${href}/${lang}/store/(${match_names})`);
+            hrefs.forEach((link)=>{
+              expect(link).to.match(r);
+            })
+            expect(hrefs).to.have.property("length",Object.keys(store_products).length);
+            return hrefs;
           })
         })
       })
     });
+    //Since we verified in previous test that "store_products" is acurate
+    //We can base further store tests on it
+    describe("[store products]",function(){
+      Object.keys(store_products).forEach((product)=>{
+        let pageLink = `${href}/${lang}/store/${product}`;
+        describe(`${store_products[product].name}`,()=>{
+          let page;
+          before(function(){
+            page = driver.get(pageLink);
+            return page; //trigger wait until it's done.
+          })
+          it("has valid title",function(){
+            return page.then(_=>driver.getTitle())
+            .then((title)=>{
+              expect(title).to.match(new RegExp(store_products[product].name,"i"));
+              /* recommended length is in fact <70 but let's not be too conservative */
+              expect(title).to.have.lengthOf.within(25,120);
+            })
+          })
+          it("Have snipcart properties",function(){
+            return page.then(_=>driver.findElement(By.css(".button.snipcart-add-item")))
+            .then((btn)=>{
+              return Promise.all([
+                btn => btn.getAttribute("data-item-name")
+                  .then((name)=>name.should.be.a.string),
+                btn => btn.getAttribute("data-item-price")
+                  .then((name)=>name.should.match(/\d+\.\d{2}/)),
+                btn => btn.getAttribute("data-item-url")
+                  .then((name)=>name.should.equal(pageLink)),
+                btn => btn.getAttribute("data-item-description")
+                  .then((name)=>name.should.be.a.string.of.length.above(20)),
+               ]);
+             });
+          });
+        });
+      });
+    })
+
+    /* Check thumbnails */
+    describe("[thumbnails]",function(){
+      [
+        `${href}/${lang}/products/`,
+        `${href}/${lang}/store/`,
+        `${href}/${lang}/posts/`,
+      ].forEach(function(link){
+        it(`on ${link}`,function(){
+          return driver.get(`${link}`)
+          .then(_=> driver.findElements(By.css(".thumbnail>a")))
+          .then((thumbs)=> get_sizes(thumbs))
+        })
+      })
+    })
   });
 })
