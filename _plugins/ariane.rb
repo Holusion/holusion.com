7cef40e67cb5a70aef3a7b995fba3ef4bb68eaf2
Sebastien Dumetz <s.dumetz@holusion.com>
2018-07-06 15:52:30 +0200

    fix ariane for dev site and a missing class in top navbar

--- _plugins/ariane.rb
+++ _plugins/ariane.rb
@@ -1,36 +1,41 @@
 module Jekyll
   module Tags
     class JekyllAriane < Liquid::Tag
+      # we use a lazy quantifier on rest to not match trailing /index.html
+      @@re = /^\/(?<prefix>(?<dev>dev\/)?(?<lang>fr|en))\/(?<rest>.*?)(index|index.html)?$/
       def render(context)
         #global site variable
         page = context.registers[:page]
         site = context.registers[:site]
         url = page["url"]
-        lang = page["lang"]
-        return if lang.nil? || lang.length == 0
-        path = url.split("/")
-        path.shift(2)
-        path.pop() if (path.last == "index" or path.last == "index.html")
-        cat_link="/#{lang}"
-
+        m = @@re.match(url)
+        if not m
+          #print "{%ariane%} called on #{url} (invalid url)\n"
+          return
+        end
+        lang = m["lang"]
+        path_parts = m["rest"].split("/")
+        fragment_link = "/#{m["prefix"]}"
         #print "#{path}\n"
-        links = path.map do |fragment|
-          cat_link+="/#{fragment}"
+        links = path_parts.map do |fragment|
+          fragment_link+="/#{fragment}"
           localized_title = if site.data["sections"][fragment] and site.data["sections"][fragment][lang]
             site.data["sections"][fragment][lang]
           else
             fragment
           end
-          %!<a href="#{cat_link}">#{localized_title}</a> !
+          %!<a href="#{fragment_link}">#{localized_title}</a> !
         end
-        return %!
-        <div class="container ariane">
-          <a href="/#{lang}">
+        links.unshift( %!<a href="/#{m["prefix"]}">dev</a> !) if m["dev"]
+
+        return %@
+        <div class="ariane">
+          <a href="/#{m["prefix"]}">
             #{site.data["sections"]["home"][lang]}
           </a>
           #{links.join("\n")}
         </div>
-        !
+        @
       end
     end
   end
