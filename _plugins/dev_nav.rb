93b65690f900a2c9e05b30cb9260737ee83a44de
Sebastien Dumetz <s.dumetz@holusion.com>
2020-08-20 09:59:45 +0200

    fix doc ordering + better visibility check (#66)
    * fix rank-based ordering for dev_nav
    
    * improve doc page visibility change

--- _plugins/dev_nav.rb
+++ _plugins/dev_nav.rb
@@ -75,20 +75,24 @@ module Jekyll
           lang = match["lang"]
           parts = match["rest"]? match["rest"].split("/") : []
           last_part = match["last"]
+
           current_hash = tree[lang]
+          
           parts.each do |part|
             #print "current hash :#{current_hash}"
-            current_hash[part] = {"children" =>{}, "title" => part } if not current_hash.has_key? part
-            current_hash = current_hash[part]["children"]
+            current_hash[part] = {:children =>{}, :title => part } if not current_hash.has_key? part
+            current_hash = current_hash[part][:children]
           end
 
           current_hash[last_part] = {
-            "title" => doc.data["title"],
-            "url" => doc.url,
-            "children"=>(current_hash.has_key?(last_part)) ? current_hash[last_part]["children"] : {},
-            "visible" => doc.data.has_key?("visible")? doc.data["visible"] : true
-          }
+            :title => doc.data["title"],
+            :rank => doc.data.has_key?("rank")? doc.data["rank"] : 0,
+            :url => doc.url,
+            :children =>(current_hash.has_key?(last_part)) ? current_hash[last_part][:children]: {},
+            :visible => doc.data.has_key?("visible")? doc.data["visible"] : true
+          } if doc.data["visible"] != false
         end
+
         return tree
       end
 
@@ -96,15 +100,15 @@ module Jekyll
         uid = path.sub("/","-")
 
         is_active = active_uri.include? path
-        title = item["title"] || path.split("/").last
-        url = item["url"] || ""
+        title = item[:title] || path.split("/").last
+        url = item[:url] || ""
 
-        return if item["visible"] == false
 
-        if item["children"].empty?
+        if item[:children].empty?
           dropdown_list = ""
         else
-          child_nodes = item["children"].map do |childKey, child|
+          sorted_children = item[:children].sort_by { |key, val| val[:rank] }
+          child_nodes = sorted_children.map do |childKey, child|
             render_item(path+"/#{childKey}", child, active_uri)
           end
 
@@ -162,8 +166,9 @@ module Jekyll
           <a href="/dev/#{lang}/">Introduction</a>
         </li>)
 
-        docs_tree[lang].each do |key, doc|
-          next if doc["children"].empty? # do not render categories with no children for now
+        sorted_localized_docs = docs_tree[lang].sort_by { |key, val| val.has_key?(:rank)? val[:rank] : 0 }
+        sorted_localized_docs.each do |key, doc|
+          next if doc[:children].empty?# do not render categories with no children for now
           html += render_item(key, doc, page["url"])
         end
   	    return html+"</ul>"
