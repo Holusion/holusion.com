bcc3e5d4017089edc7b5607bd840c27e8854c0c1
Pakrett <sarah.menouar@gmail.com>
2021-07-05 12:01:56 +0200

    Product page and interaction page in dark theme (#134)
    * fix bugs in observer.js. Add capability to handle multiple observed sections
    
    * clearer css for fixed-background grid
    
    fix backdrop-filter for safari (add prefix)
    
    * WIP prototype pricing in dark theme
    
    * prototype pricing in white
    
    * wip white card price
    
    * prototype new pricing section with white cards
    
    * wip product page dark mode
    
    * display products in slides
    
    * test slides with photos and drawings
    
    * fix some title margin and missing closed div
    
    * test mise en page interaction
    
    * test interaction page with light background
    
    * layout interaction page
    
    * add price in card products
    
    * add links in products page
    
    * delete obsolete tests
    
    * add products illustrations
    
    * add pixel slide illustration
    
    * update product price display
    
    * update slide products images
    
    * update product header image
    
    * fix product header image size
    
    * fix margin wider than screen
    
    * add english translation
    
    * delete unused images
    
    * update gamme sketch and put back unique project slide
    
    * add CTA button catalog and add alt text
    
    Co-authored-by: Sebastien DUMETZ <s.dumetz@holusion.com>

--- static/js/observer.js
+++ static/js/observer.js
@@ -1,49 +1,61 @@
 
 window.addEventListener("load", function(){
   'use strict';
-  const contents = Array.prototype.slice.call(document.querySelectorAll(".section .slide.content > div"));
-  const slides = document.querySelectorAll(".section .sticky-content .slides img");
-  const allSlides = contents.map((_, i)=>i);
-  //Read DOMNodes only once to optimize repaints
-  const slidesIndices = Array.prototype.map.call(slides, (img)=>{
-    if(img.dataset.slides === "all") return allSlides;
-    else if(typeof img.dataset.slides !== "undefined"){
-      let indices = [], m, re=/(?:(\d+)(?:-(\d+))?)/g;
-      while(m = re.exec(img.dataset.slides)){
-        for(let i=m[1]; i <= (m[2] || m[1]); i++){
-          indices.push(parseInt(i));
+  function observeSection(section){
+    const contents = Array.prototype.slice.call(section.querySelectorAll(".slide.content > div"));
+    const slides = section.querySelectorAll([
+      "img",
+      "video",
+      "picture>img",
+      "div",
+    ].map(s=> ".sticky-content .slides > "+s).join(", "));
+
+    if(!contents.length) console.log("No slide-content on this page. Consider not including observer.js");
+    if(!slides.length) console.log("No slides on this page. Consider not including observer.js");
+  
+    const allSlides = contents.map((_, i)=>i);
+    //Read DOMNodes only once to optimize repaints
+    const slidesIndices = Array.prototype.map.call(slides, (img)=>{
+      if(img.dataset.slides === "all") return allSlides;
+      else if(typeof img.dataset.slides !== "undefined"){
+        let indices = [], m, re=/(?:(\d+)(?:-(\d+))?)/g;
+        while(m = re.exec(img.dataset.slides)){
+          for(let i=m[1]; i <= (m[2] || m[1]); i++){
+            indices.push(parseInt(i));
+          }
+        }
+        return indices;
+      }else{
+        return [Array.prototype.indexOf.call(img.parentNode.children, img)];
+      }
+    });
+    let thresholdValue = window.matchMedia("(max-width: 1200px)").matches ? 0.1 : 0.5;
+  
+    let o = new IntersectionObserver((entries)=>{
+      for (let entry of entries){
+        if(!entry.isIntersecting) continue;
+        const slideIndex = contents.indexOf(entry.target);
+        for (let idx = 0; idx < slides.length; idx++){
+          const img = slides[idx];
+          if(slidesIndices[idx].indexOf(slideIndex) !== -1){
+            img.style.opacity = 1;
+          }else{
+            img.style.opacity = 0;
+          }
         }
       }
-      return indices;
-    }else{
-      return [Array.prototype.indexOf.call(img.parentNode.children, img)];
+    }, {
+      threshold:[thresholdValue]
+    });
+  
+    for(let c of contents){
+      o.observe(c);
     }
-  });
-  //const backgrounds = document.querySelectorAll(".section .slides-back > img, .section .slides-front > picture");
-  const stickyContent = document.querySelectorAll(".section .sticky-content > img, .section .sticky-content > picture");
-  let thresholdValue = window.matchMedia("(max-width: 1200px)").matches ? 0.1 : 0.5;
-
-  if(!contents.length) console.log("No slide-content on this page. Consider not including observer.js");
-  if(!slides.length) console.log("No slides on this page. Consider not including observer.js");
-
-  let o = new IntersectionObserver((entries)=>{
-    for (let entry of entries){
-      if(!entry.isIntersecting) continue;
-      const slideIndex = contents.indexOf(entry.target);
-      for (let idx = 0; idx < slides.length; idx++){
-        const img = slides[idx];
-        if(slidesIndices[idx].indexOf(slideIndex) !== -1){
-          img.style.opacity = 1;
-        }else{
-          img.style.opacity = 0;
-        }
-      }
-    }
-  }, {
-    threshold:[thresholdValue]
-  });
-
-  for(let c of contents){
-    o.observe(c);
   }
+
+  const fixed_background_sections = document.querySelectorAll(".section .fixed-background");
+  for(let {parentNode: section} of fixed_background_sections){
+    observeSection(section);
+  }
+  
 });
