632cc06428a7244ae52a75d037a4903db32fcddb
Sebastien DUMETZ <s.dumetz@holusion.com>
2021-12-14 11:06:48 +0100

    remove duplicate front matter in localized content. Add a test to prevent front-matter duplication

--- scripts/integration.test.js
+++ scripts/integration.test.js
@@ -3,88 +3,99 @@ const expect = require("chai").expect;
 const {resolve, relative} = require("path");
 const fs = require("fs").promises;
 const https = require('https');
-const findFiles = require("./utils/findFiles");
+
+const yaml = require('js-yaml');
+
+const {findFiles, findFrontMatters} = require("./utils/findFiles");
 
 const prettyPrint = require("./utils/prettyPrint");
 
 const basePath = resolve(__dirname, "..")
 
-describe("static & assets files", ()=>{
-  let files, sizes;
-  const folders = ["_assets", "static"];
+describe("integration tests", function(){
   const image_re = /\.(?:png|jpe?g|webp)$/i;
-  before(async ()=>{
-    files = (await Promise.all(folders.map(f => findFiles(resolve(basePath, f))))).flat();
-    expect(files.length).to.be.above(100);
-    sizes = await Promise.all(files.map(async file =>{
+  const folders = ["_assets", "static"];
+  let staticFiles, sizes;
+  before(async function(){
+    staticFiles = (await Promise.all(folders.map(f => findFiles(resolve(basePath, f))))).flat()
+    sizes = await Promise.all(staticFiles.map(async file =>{
       const stats = await fs.stat(file);
       return stats.size;
     }));
-  });
-  describe("size checks", ()=>{
-    it("large files", async ()=>{
-      const max_size = 1 *1024 /*1 kb*/ * 1024 /*1 mb*/ *64; //Max allowed size in octets
-      const bad_files = files.filter((f, idx)=>{
-        return max_size < sizes[idx];
+  })
+
+  describe("static & assets files", ()=>{
+    it("expect many files", function(){
+      expect(staticFiles.length).to.be.above(100);
+    });
+
+    describe("size checks", ()=>{
+      it("large files", async ()=>{
+        const max_size = 1 *1024 /*1 kb*/ * 1024 /*1 mb*/ *64; //Max allowed size in octets
+        const bad_files = staticFiles.filter((f, idx)=>{
+          return max_size < sizes[idx];
+        });
+        expect(bad_files.length).to.be.below(10, `Found ${bad_files.length} files above size limit.`);
+        expect(bad_files.length).to.equal(0, `Expected no file to be above ${Math.round(max_size/(1024*1024))} mb. Bad files :`
+          + prettyPrint(bad_files));
       });
-      expect(bad_files.length).to.be.below(10, `Found ${bad_files.length} files above size limit.`);
-      expect(bad_files.length).to.equal(0, `Expected no file to be above ${Math.round(max_size/(1024*1024))} mb. Bad files :`
-        + prettyPrint(bad_files));
+    
+      it("large images", async ()=>{
+        const max_size = 1 *1024* 1024; //Max allowed size in octets
+        const bad_images = staticFiles.filter((f, idx)=>{
+          if(!image_re.test(f)) return false;
+          return max_size < sizes[idx];
+        });
+        expect(bad_images.length).to.be.below(10, `Found ${bad_images.length} images above size limit.`
+        + `\nList bad images with : \n\t\`find ${folders.join(" ")} -size +${Math.round(max_size/1024)}k \\( -iname "*.png" -o -iname "*.jpg" \\)\``
+        + "\n");
+        expect(bad_images.length).to.equal(0, `Expected no image to be above ${Math.round(max_size/1024)} kb. Bad images :`
+          + prettyPrint(bad_images))
+      });
     });
   
-    it("large images", async ()=>{
-      const max_size = 1 *1024* 1024; //Max allowed size in octets
-      const bad_images = files.filter((f, idx)=>{
-        if(!image_re.test(f)) return false;
-        return max_size < sizes[idx];
-      });
-      expect(bad_images.length).to.be.below(10, `Found ${bad_images.length} images above size limit.`
-      + `\nList bad images with : \n\t\`find ${folders.join(" ")} -size +${Math.round(max_size/1024)}k \\( -iname "*.png" -o -iname "*.jpg" \\)\``
-      + "\n");
-      expect(bad_images.length).to.equal(0, `Expected no image to be above ${Math.round(max_size/1024)} kb. Bad images :`
-        + prettyPrint(bad_images))
+    describe("format check", ()=>{
+      it(`mp4 videos have "faststart" moov atom`, async ()=>{
+        const re = /\.(?:mp4)$/i
+        const videos = staticFiles.filter((f, idx)=>re.test(f));
+  
+        const headers = await Promise.all(videos.map( async video => {
+          const handle = await fs.open(video, "r");
+          let str;
+          try{
+            let buf = Buffer.allocUnsafe(100);
+            await handle.read(buf,0, 100);
+            str = buf.toString();
+          }finally{
+            handle.close();
+          }
+          return str;
+        }));
+        const bad_moov = videos.filter((video, idx)=> {
+          const movIndex = headers[idx].indexOf("moov");
+          const mdatIndex = headers[idx].indexOf("mdat");
+          return movIndex === -1 || (mdatIndex != -1 && mdatIndex < movIndex);
+        });
+        expect(bad_moov).to.have.property("length", 0, `moov fragment "faststart" not set on videos :`
+          + prettyPrint(bad_moov)
+          + `Fix with : \n\t\`(export FILE="<file>" && ffmpeg -y -i "$FILE" -c copy -movflags +faststart "/tmp/$(basename "$FILE")" && mv "/tmp/$(basename "$FILE")" "$FILE")\``
+          + "\n");
+      })
     });
-  });
+  })
 
-  describe("format check", ()=>{
-    it(`mp4 videos have "faststart" moov atom`, async ()=>{
-      const re = /\.(?:mp4)$/i
-      const videos = files.filter((f, idx)=>re.test(f));
-
-      const headers = await Promise.all(videos.map( async video => {
-        const handle = await fs.open(video, "r");
-        let str;
-        try{
-          let buf = Buffer.allocUnsafe(100);
-          await handle.read(buf,0, 100);
-          str = buf.toString();
-        }finally{
-          handle.close();
-        }
-        return str;
-      }));
-      const bad_moov = videos.filter((video, idx)=> {
-        const movIndex = headers[idx].indexOf("moov");
-        const mdatIndex = headers[idx].indexOf("mdat");
-        return movIndex === -1 || (mdatIndex != -1 && mdatIndex < movIndex);
-      });
-      expect(bad_moov).to.have.property("length", 0, `moov fragment "faststart" not set on videos :`
-        + prettyPrint(bad_moov)
-        + `Fix with : \n\t\`(export FILE="<file>" && ffmpeg -y -i "$FILE" -c copy -movflags +faststart "/tmp/$(basename "$FILE")" && mv "/tmp/$(basename "$FILE")" "$FILE")\``
-        + "\n");
-    })
-  });
   describe("contents checks", ()=>{
-    let contents, contentFiles;
+    let contentFiles;
     const siteDir = resolve(basePath, "_site");
     before(async ()=>{
-      contentFiles = await findFiles(siteDir, {match:/\.(?:html|css)$/});
-      contents = (await Promise.all(contentFiles.map(f=> fs.readFile(f, {encoding:"utf8"}))));
+      const files = await findFiles(siteDir, {match:/\.(?:html|css)$/});
+      const contents = (await Promise.all(files.map(f=> fs.readFile(f, {encoding:"utf8"}))));
+      contentFiles = files.map((filepath, index)=> ({filepath, index, content: contents[index]}));
     });
     describe("usage check", ()=>{
       it("images",async ()=>{
-        const images = files.filter(i=>image_re.test(i));
-        expect(images).to.have.property("length").above(100);
+        const images = staticFiles.filter(i=>image_re.test(i));
+        expect(images, "Expected many images").to.have.property("length").above(100);
         let not_found = images.filter((image)=>{
           let rel = relative(basePath, image)
           if(rel.indexOf("_assets") === 0){
@@ -92,7 +103,7 @@ describe("static & assets files", ()=>{
           }
           const parts = rel.split("/");
           rel = `${parts.slice(0, -1).join("/")}/${encodeURIComponent(parts[parts.length -1])}`;
-          return !contents.some(c=> c.indexOf(rel) !== -1);
+          return !contentFiles.some(c=> c.content.indexOf(rel) !== -1);
         });
         expect(not_found).to.have.property("length", 0, `Some images are not used anywhere :`
         + prettyPrint(not_found.slice(0,10))
@@ -102,14 +113,13 @@ describe("static & assets files", ()=>{
     });
   
     describe("embeds check", ()=>{
- 
       it("gdoc", async ()=>{
-        const matches = contents.map(c=>{
-          return Array.from(c.matchAll(/div id="dgoc-placeholder" data-id="([^"]*)"/mg));
+        const matches = contentFiles.map(c=>{
+          return Array.from(c.content.matchAll(/div id="dgoc-placeholder" data-id="([^"]*)"/mg));
         })
         .map((matches, fileIndex)=> matches.map(m=> ({
           id:m[1], 
-          file: contentFiles[fileIndex],
+          file: contentFiles[fileIndex].filepath,
           url: `https://docs.google.com/document/d/${m[1]}/export?format=pdf`
         })))
         .filter(m=> 0 <m.length)
@@ -150,5 +160,89 @@ describe("static & assets files", ()=>{
       });
     });
   })
-  
+
+  describe("front matter checks", function(){
+
+    ["store", "products"].forEach((folder=>{
+      describe(`/en/${folder}/`, function(){
+        let en_files, fr_files;
+        before(async function(){
+          en_files = await findFrontMatters(resolve(basePath, `en/${folder}`), {match:/(?!index)\.(?:html|md)$/});
+          fr_files = await findFrontMatters(resolve(basePath, `fr/${folder}`), {match:/(?!index)\.(?:html|md)$/});
+        });
+        it("filter matches files", function(){
+          expect(en_files).to.have.property("length").above(2);
+        });
+        it("every file has a yaml front matter", function(){
+          for(let file of en_files){
+            expect(file.frontMatter, `${file.filepath} has no yaml front matter`).to.have.property("length").above(0);
+          }
+        });
+        describe("localization compare", function(){
+          let comparedFiles;
+          before(function(){
+            comparedFiles = en_files.map(file=>{
+              let fr_filePath = file.filepath.replace("/en/", "/fr/");
+              let {frontMatter:fr_frontMatter} = fr_files.find(fr_file=> {
+                return fr_file.filepath === fr_filePath
+              });
+              return {...file, fr_filePath, fr_frontMatter};
+            });
+          });
+          it("every file has a french counterpart", function(){
+            for(let file of comparedFiles){
+              expect(file.fr_frontMatter).to.be.ok;
+              expect(file.fr_frontMatter).to.have.property("length").above(0);
+            }
+          });
+          
+          it("checks for duplicate keys", async function(){
+            //Deeply compare hashes for any incorrect specification
+            function deep_compare(en, fr, propchain){
+              let errors = []
+              for(let key in en){
+                let keychain = propchain?`${propchain}.${key}`: key;
+                if(!fr.hasOwnProperty(key) ){ 
+                  //Keys that are allowed a less strict check
+                  if( ["layout", "lang", "rank",, "published" ].indexOf(key) == -1) continue;
+                  errors.push(`property ${
+                    keychain
+                  } should also be provided in french (or deleted)`);
+                }else if(typeof en[key] != typeof fr[key]){
+                  errors.push(`property ${
+                    keychain
+                  } has different type (${
+                    typeof en[key]
+                  }) in english than in french (${
+                    typeof fr[key]
+                  })`);
+                }else if(typeof en[key] === "object"){
+                  errors.push(...deep_compare(en[key], fr[key], keychain));
+                }else if(en[key] === fr[key] && ["layout", "title", "rank", "published"].indexOf(key) == -1) {
+                  errors.push(`property [${keychain}] should be translated\n    or deleted from the english page (content duplication)`);
+                }
+              }
+              return errors;
+            }
+
+            let errors = [], totalErrorCount = 0;
+            for(let file of comparedFiles){
+              let header = yaml.load(file.frontMatter);
+              let fr_header = yaml.load(file.fr_frontMatter);
+              expect(header).to.be.a("object");
+              expect(fr_header, `Bad header for ${file.fr_filePath}. From : ${file.fr_frontMatter}`).to.be.a("object");
+              let fileErrors = deep_compare(header, fr_header);
+              if(0 < fileErrors.length){
+                errors.push(`IN ${relative(basePath,file.filepath)} :\n  ${fileErrors.join("\n  ")}`)
+                totalErrorCount += fileErrors.length;
+              }
+            }
+            if(0 < errors.length){
+              expect.fail(`${totalErrorCount} Errors : \n${errors.join("\n")}`);
+            }
+          })
+        });
+      });
+    }));
+  })
 })
--- scripts/utils/findFiles.js
+++ scripts/utils/findFiles.js
@@ -1,10 +1,10 @@
 'use strict';
 const {join} = require('path');
-const {readdir} = require("fs").promises;
+const fs = require("fs").promises;
+const yaml = require("js-yaml");
 
-
-module.exports = async function findFiles(dir, {match}= {}){
-  const entries = await readdir(dir, {withFileTypes: true});
+async function findFiles(dir, {match}= {}){
+  const entries = await fs.readdir(dir, {withFileTypes: true});
   const chunks = await Promise.all(entries.map((f)=>{
     if(f.isDirectory()) return findFiles(join(dir, f.name));
     else if(f.isFile()) return [join(dir, f.name)];
@@ -15,3 +15,23 @@ module.exports = async function findFiles(dir, {match}= {}){
   else if(match instanceof RegExp) return files.filter(f=> match.test(f));
   else return files.filter(f => f.indexOf(match) != -1);
 }
+
+function getFrontMatter(contentBuf){
+  let yaml_start = contentBuf.indexOf("---");
+  if(yaml_start < 0) return Buffer.alloc(0);
+  let yaml_end = contentBuf.indexOf('---', yaml_start+3);
+  if(yaml_start < 0) return Buffer.alloc(0);
+  return contentBuf.slice(yaml_start+3, yaml_end);
+}
+
+async function findFrontMatters(dir, opts){
+  const allFiles = await findFiles(dir, opts);
+  return await Promise.all(allFiles.map(async filepath=> {
+    let contentBuf = await fs.readFile(filepath, {});
+    let frontMatter = getFrontMatter(contentBuf);
+    return {filepath, frontMatter};
+  }));
+}
+
+
+module.exports = {findFiles, findFrontMatters};
